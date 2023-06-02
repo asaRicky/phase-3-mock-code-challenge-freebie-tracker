ae33b9d114c0ec9babfe6ff1dc692bcfca8f9dbc
asaRicky <derrickomondi208@gmail.com>
2023-06-02 13:35:19 +0300

    completed

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,10 @@
 class Company < ActiveRecord::Base
-
+   has_many :freebies
+   has_many :devs, through: :freebies
+   def give_freebie(dev, item_value, value)
+       Freebie.create(item_value: value, item_name: item_name, value: value, company_id: self.id, dev_id: dev.id)
+   end
+   def self.oldest_company
+    company.all.order(:founding_year).first
+   end
 end
